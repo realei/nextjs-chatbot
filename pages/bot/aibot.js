b582ff14048f72331cdf349fedcdc01787b4da9c
Lei Wang <wanglei.okay@gmail.com>
2024-03-02 22:42:02 +0200

    chore

--- pages/bot/aibot.js
+++ pages/bot/aibot.js
@@ -67,7 +67,7 @@ export default function AIBot() {
         inputStyle={{ // This will style the input area
           margin: '1em', // Add some margin around the input
         }}
-        bubbleStyle={{ whiteSpace: 'pre' }}
+        bubbleStyle={{ whiteSpace: 'pre-wrap', paddingLeft: '20px'}}
       />
     </div>
   );
